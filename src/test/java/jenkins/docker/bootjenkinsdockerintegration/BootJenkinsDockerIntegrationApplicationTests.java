0f8443fe5c4c73e54cba2938bdca2ad49ba5f3be
Pritam Kumar Samal <pritam.samal@s-force.org>
2020-02-21 21:03:43 +0530

    added logger to the testcase

--- src/test/java/jenkins/docker/bootjenkinsdockerintegration/BootJenkinsDockerIntegrationApplicationTests.java
+++ src/test/java/jenkins/docker/bootjenkinsdockerintegration/BootJenkinsDockerIntegrationApplicationTests.java
@@ -3,13 +3,18 @@ package jenkins.docker.bootjenkinsdockerintegration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootJenkinsDockerIntegrationApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(BootJenkinsDockerIntegrationApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
+		logger.info("Running the Test Cases");
 		assertEquals(true, true);
 	}
 
